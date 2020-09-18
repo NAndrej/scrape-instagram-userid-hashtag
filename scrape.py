b121643d4a58718f3d4f329dbb90209b2c4a8e5e
Andrej Naumovski <smo.okmvm@gmail.com>
2020-09-18 15:22:11 +0200

    scrape_users, scrolling, downloading

--- scrape.py
+++ scrape.py
@@ -4,18 +4,31 @@ from selenium import webdriver
 import os
 import random
 import csv
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
 
 class Scraper:
 
     def __init__(self):
-        self.instagram_url = "https://www.instagram.com/{}"
+
         self.driver = None
+        self.output = None
+
+        # Constants
+        self.userid_instagram_url = "https://www.instagram.com/{}"
+        self.hashtag_instagram_url = "https://www.instagram.com/explore/tags/{}/"
+
+        # Constants - Credentials
+        self.log_in_url = "https://www.instagram.com/"
+        self.instagram_username = "sylisdavern7"
+        self.instagram_password = "password#@!"
 
         with open("user_agents.txt", "r") as f:
             user_agents = f.readlines()
             self.user_agents = [i.strip().strip("\n") for i in user_agents]
 
-        self.output = None
 
     def write_csv(self,file,row):
         with open(file, "a", newline='', encoding="utf-8") as f:
@@ -32,31 +45,74 @@ class Scraper:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("user-agent="+str(user_agent))
         # chrome_options.add_argument("proxy-server="+str(next(self.proxypool)))
-        chrome_options.add_argument("headless")
+        # chrome_options.add_argument("headless")
         chrome_options.add_argument("no-sandbox")
+        chrome_options.add_argument("--incognito")
         chrome_options.add_argument("start-maximized")
         chrome_options.add_argument("disable-logging")
-        chrome_options.add_argument("log-level=3")
+        # chrome_options.add_argument("log-level=3")
         
         self.driver = webdriver.Chrome(options=chrome_options,executable_path="c_driver/chromedriver.exe")
 
     def setup(self):
+        self.get_driver()
+        self.log_in()
         self.output = os.path.join(os.getcwd(), "output.csv")
         self.write_csv(self.output,["ID","Data Type","Username","Number of followers", "Number of following", "Description", "Number of Likes","URL"])
 
-    def scrape_userid(self):
-        self.get_driver()
-        username = "ronaldinho"
-        url = self.instagram_url.format(username)
-        self.driver.get(url)
+    # def scroll_down(self,min_times,max_times):
+    #     num_times = random.randint(min_times,max_times)
+    #     for i in range(num_times):
+    #         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #         time.sleep(3)
 
+    def download_image(self,image,id):
+        print("Downloading image {} ............".format(image))
+
+        response = requests.get(image, timeout=5)
+        
+        img_path = "images/"+str(id)+".jpg"
+
+        #Writing image to file
+        file = open(img_path, "wb")
+        file.write(response.content)
+        file.close()
+        print("Image successfully downloaded....")
+
+    def scrape_userid(self):
+        # try: 
+        username = "mikeescamilla"
+        url = self.userid_instagram_url.format(username)
+        self.driver.get(url)
+        print(url)
+
+        #Initial posts, without scrolling
         posts = self.driver.find_elements_by_tag_name("a")
-        posts = [post for post in posts if "https://www.instagram.com/p/" in post.get_attribute("href")]
-        print("Post links fetched")
+        posts = [post.get_attribute("href") for post in posts if "https://www.instagram.com/p/" in post.get_attribute("href")]
+
+        #Scrolling down, appending new posts to posts list
+        for i in range(5):
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(5)
+            new_posts = self.driver.find_elements_by_tag_name("a")
+            new_posts = [new_post.get_attribute("href") for new_post in new_posts if "https://www.instagram.com/p/" in new_post.get_attribute("href")]
+
+            last_ind = new_posts.index(posts[len(posts)-1])
+            current_new_posts = new_posts[-(len(new_posts)-last_ind-1):]
+            
+            for current_new_post in current_new_posts:
+                if "https://www.instagram.com/p/" in current_new_post:
+                    posts.append(current_new_post)
+
+            print("{} Posts fetched".format(len(posts)))
+
+
+        time.sleep(3)
+        print("Post links fetched completely")
+        print("{} Posts fetched".format(len(posts)))
 
         num_followers = self.driver.find_elements_by_class_name('g47SY')[1].text
         num_following = self.driver.find_elements_by_class_name('g47SY')[2].text
-
 
         print("Iterating trough posts...")
         id = 0
@@ -64,9 +120,7 @@ class Scraper:
             id+=1
             data_type = ""
             description = ""
-
-
-            post_url = post.get_attribute("href")
+            post_url = post
             self.get_driver()
             self.driver.get(post_url)
             print("Scraping: {}".format(post_url))
@@ -79,7 +133,6 @@ class Scraper:
                 print("Description is empty")
             else:
                 description = desc.text
-
 
             #VIDEO CHECK
             video_button = self.driver.find_elements_by_class_name('tWeCl')
@@ -104,18 +157,51 @@ class Scraper:
 
 
             #IMAGE
-            
-
             data_type = "Image"
             row = [id,data_type,username,num_followers,num_following,description,num_likes,post_url]
             self.write_csv(self.output,row)
-
+            
             image = self.driver.find_element_by_xpath("//div[@class='KL4Bh']/img").get_attribute("src")
+
+            self.download_image(image,id)
             print(image)
 
+        # except Exception as e:
+        #     self.driver.quit()
+        #     print('except happened')
+        #     print(e)
+        #     exit()
 
+        self.driver.quit()
+        print('done')
+        exit()
 
+    def log_in(self):
+        try:
+            print("Logging in")
+            self.driver.get(self.log_in_url)
+            self.driver.implicitly_wait(5)
+
+            username_field = self.driver.find_element_by_name('username')
+            password_field = self.driver.find_element_by_name('password')
+            log_in_button = self.driver.find_elements_by_xpath("//div[text()='Log In']")[0]
+
+            username_field.send_keys(self.instagram_username)  
+            password_field.send_keys(self.instagram_password)
+
+            log_in_button.click()
+
+            self.driver.implicitly_wait(5)
+
+            if self.driver.find_elements_by_css_selector("[aria-label=Home]")[0].get_attribute('fill'):
+                print('logged in!')
+        except Exception as e:
+            print("Exception")
+            self.driver.quit()
+            exit()
+        
 if __name__=="__main__":
     s = Scraper()
     s.setup()
     s.scrape_userid()
+    # s.scrape_hashtag()
