55c5837ae30202980460fca74fed274ce8107dd6
vidakbeshlikj <vidakbeshlikj@gmail.com>
2020-09-20 20:40:39 +0200

    ltst

--- scrape.py
+++ scrape.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 import os
 import random
 import csv
+import uuid
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
@@ -15,10 +16,14 @@ class Scraper:
 
         self.driver = None
         self.output = None
+        self.hashtag_posts = []
+        self.user_posts = []
+        self.generated_uuid = uuid.uuid4()
 
         # Constants
         self.userid_instagram_url = "https://www.instagram.com/{}"
         self.hashtag_instagram_url = "https://www.instagram.com/explore/tags/{}/"
+        self.post_base_url = "https://www.instagram.com/p/"
 
         # Constants - Credentials
         self.log_in_url = "https://www.instagram.com/"
@@ -28,7 +33,6 @@ class Scraper:
         with open("user_agents.txt", "r") as f:
             user_agents = f.readlines()
             self.user_agents = [i.strip().strip("\n") for i in user_agents]
-
 
     def write_csv(self,file,row):
         with open(file, "a", newline='', encoding="utf-8") as f:
@@ -57,27 +61,36 @@ class Scraper:
     def setup(self):
         self.get_driver()
         self.log_in()
-        self.output = os.path.join(os.getcwd(), "output.csv")
+        self.output = os.path.join(os.getcwd(), "{}.csv".format(self.generated_uuid))
         self.write_csv(self.output,["ID","Data Type","Username","Number of followers", "Number of following", "Description", "Number of Likes","URL"])
 
-    # def scroll_down(self,min_times,max_times):
-    #     num_times = random.randint(min_times,max_times)
-    #     for i in range(num_times):
-    #         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    #         time.sleep(3)
+    def scroll_down(self, scrape_type):
+        if scrape_type == 'hashtag':
+            for i in range(random.randint(1, 10)):
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                self.hashtag_posts.extend(post.get_attribute("href") for post in self.driver.find_elements_by_tag_name("a") if "https://www.instagram.com/p/" in post.get_attribute("href") and post.get_attribute("href") not in self.hashtag_posts)
+            print("{} hashtag posts fetched".format(len(self.hashtag_posts)))
+
+        if scrape_type == 'user':
+            for i in range(random.randint(1, 10)):
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                self.user_posts.extend(post.get_attribute("href") for post in self.driver.find_elements_by_tag_name("a") if "https://www.instagram.com/p/" in post.get_attribute("href") and post.get_attribute("href") not in self.user_posts)
+            print("{} user posts fetched".format(len(self.user_posts)))
 
     def download_image(self,image,id):
-        print("Downloading image {} ............".format(image))
+        print("Downloading image {}".format(image))
 
         response = requests.get(image, timeout=5)
         
-        img_path = "images/"+str(id)+".jpg"
+        img_path = "{}-".format(self.generated_uuid) + str(id) + ".jpg"
 
         #Writing image to file
         file = open(img_path, "wb")
         file.write(response.content)
         file.close()
-        print("Image successfully downloaded....")
+        print("Image successfully downloaded...")
 
     def scrape_userid(self):
         # try: 
@@ -121,7 +134,7 @@ class Scraper:
             data_type = ""
             description = ""
             post_url = post
-            self.get_driver()
+
             self.driver.get(post_url)
             print("Scraping: {}".format(post_url))
 
@@ -161,10 +174,8 @@ class Scraper:
             row = [id,data_type,username,num_followers,num_following,description,num_likes,post_url]
             self.write_csv(self.output,row)
             
-            image = self.driver.find_element_by_xpath("//div[@class='KL4Bh']/img").get_attribute("src")
-
+            image = self.driver.find_element_by_class_name('FFVAD').get_attribute('srcset').split(' ')[0]
             self.download_image(image,id)
-            print(image)
 
         # except Exception as e:
         #     self.driver.quit()
@@ -194,14 +205,92 @@ class Scraper:
             self.driver.implicitly_wait(5)
 
             if self.driver.find_elements_by_css_selector("[aria-label=Home]")[0].get_attribute('fill'):
-                print('logged in!')
+                print('Logged in')
         except Exception as e:
             print("Exception")
             self.driver.quit()
             exit()
-        
+
+    def scrape_hashtag_posts(self):
+        self.hashtag_posts = []
+        self.driver.get(self.hashtag_instagram_url.format("hydrogenwater"))
+        self.driver.implicitly_wait(5)
+        self.hashtag_posts = [post.get_attribute("href") for post in self.driver.find_elements_by_tag_name("a") if "https://www.instagram.com/p/" in post.get_attribute("href")]
+        self.scroll_down('hashtag')
+        self.process_scraped_posts(self.hashtag_posts)
+
+    def process_scraped_posts(self, posts):
+
+        num_followers = 'unknown'
+        num_following = 'unknown'
+        username = 'unknown'
+
+        # Number of followers and following     
+        try:
+            num_followers = self.driver.find_elements_by_class_name('g47SY')[1].text
+            num_following = self.driver.find_elements_by_class_name('g47SY')[2].text
+        except:
+            print('Number of followers and following unavailable')
+
+        id = 0
+        for post in posts:
+            id += 1
+            data_type = ""
+            description = ""
+            post_url = post
+
+            print("Scraping: {}".format(post_url))
+
+            self.driver.get(post_url)
+
+            # Username
+            try:
+                username = self.driver.find_element_by_xpath("//div[@class='e1e1d']/span[@class='Jv7Aj  MqpiF  ' and 1]/a[@class='sqdOP yWX7d     _8A5w5   ZIAjV ' and 1]").text
+            except:
+                username = 'unavailable'
+
+            # Likes
+            try:
+                num_likes = self.driver.find_element_by_xpath("//button[@class='sqdOP yWX7d     _8A5w5    ']").text.rstrip(" likes")
+            except:
+                num_likes = 'unavailable'
+
+            # Description
+            try:
+                desc = self.driver.find_element_by_xpath("//div[@class='C4VMK']/span")
+            except Exception as e:
+                print("Description is empty")
+            else:
+                description = desc.text
+
+            # Video
+            video_button = self.driver.find_elements_by_class_name('tWeCl')
+            if len(video_button) != 0:
+                data_type = "Video"
+                num_likes = ""
+                row = [id,data_type,username,num_followers,num_following,description,num_likes,post_url]
+                self.write_csv(self.output,row)
+                continue
+
+            # Slideshow
+            slideshow = self.driver.find_elements_by_xpath("//div[@class='    coreSpriteRightChevron  ']")
+            if len(slideshow) != 0:
+                data_type = "Slideshow"
+                row = [id,data_type,username,num_followers,num_following,description,num_likes,post_url]
+                self.write_csv(self.output,row)
+                continue
+
+            # Image
+            data_type = "Image"
+            row = [id,data_type,username,num_followers,num_following,description,num_likes,post_url]
+            self.write_csv(self.output,row)
+            
+
+            image = self.driver.find_element_by_class_name('FFVAD').get_attribute('srcset').split(' ')[0]
+            self.download_image(image, id)
+
 if __name__=="__main__":
     s = Scraper()
     s.setup()
+    # s.scrape_hashtag_posts()
     s.scrape_userid()
-    # s.scrape_hashtag()
